2db169c981075f5c90fa9dac39f48ae33b6dcacc
Nico <nrafael@gmail.com>
2026-07-13 17:19:06 -0700

    Updated SQL and Project Documentation

--- sql/nicosys-portfolio-healthcare-sql.sql
+++ sql/nicosys-portfolio-healthcare-sql.sql
@@ -28,6 +28,21 @@ Order By
 ;
 
 
+--Hours experiencing the highest patient volume
+Select
+	  count(arrival_date) as number_of_patients
+	, arrival_date
+	, arrival_hour
+From
+	er_visits
+Group By
+	  arrival_hour
+	, arrival_date
+Order By
+	number_of_patients desc
+;
+
+--Correlation between Severity Level and Wait Time | 1052 Low Severity | 439 Low Severity >=120 | 613 Low Severity <120 | Med Severity 1158, >=120: 13, <120:1145 | 452 | .97/.03
 Select
 	  visit_id
 	, patient_id
@@ -40,9 +55,28 @@ Select
 	, admitted
 From
 	er_visits
+Where
+	severity_level = 'Low'
+	and wait_time_minutes <120
 Order By
 	wait_time_minutes
 ;
+
+
+Select
+	  visit_id
+	, patient_id
+	, arrival_date
+	, arrival_day
+	, arrival_hour
+	, severity_level
+	, wait_time_minutes
+	, length_of_stay_hours
+	, admitted
+From
+	er_visits
+;
+
 
 
 
